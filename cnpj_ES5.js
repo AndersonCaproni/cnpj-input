4aa228d94eb1840d28837dfd27853ce66806e9c5
anderson <anderson.git@gmail.com>
2026-03-05 11:55:43 -0300

    Refatoração do ES5 para o padrão SEPRO

--- cnpj_ES5.js
+++ cnpj_ES5.js
@@ -1,56 +1,51 @@
-var CnpjES5 = (function() {
-
-  var TAMANHO_CNPJ_SEM_DV = 12;
-  var REGEX_CNPJ_SEM_DV = /^([A-Z\d]){12}$/;
-  var REGEX_CNPJ = /^([A-Z\d]){12}(\d){2}$/;
-  var REGEX_CARACTERES_MASCARA = /[./-]/g;
-  var CNPJ_ZERADO = "00000000000000";
-  var PESOS_DV = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+var CnpjES5 = (function () {
+  var tamanhoCNPJSemDV = 12;
+  var regexCNPJSemDV = /^([A-Z\d]){12}$/;
+  var regexCNPJ = /^([A-Z\d]){12}(\d){2}$/;
+  var regexCaracteresMascara = /[./-]/g;
+  var regexCaracteresNaoPermitidos = /[^A-Z\d./-]/i;
+  var valorBase = "0".charCodeAt(0);
+  var pesosDV = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+  var cnpjZerado = "00000000000000";
 
   function removeMascaraCNPJ(cnpj) {
-    return cnpj.replace(REGEX_CARACTERES_MASCARA, "");
+    return cnpj.replace(regexCaracteresMascara, "");
   }
 
   function calculaDV(cnpj) {
-    var cnpjSemMascara = removeMascaraCNPJ(cnpj).toUpperCase();
-
-    if (REGEX_CNPJ_SEM_DV.test(cnpjSemMascara) &&
-        cnpjSemMascara !== CNPJ_ZERADO.substring(0, TAMANHO_CNPJ_SEM_DV)) {
-
-      var somatorioDV1 = 0;
-      var somatorioDV2 = 0;
-
-      for (var i = 0; i < TAMANHO_CNPJ_SEM_DV; i++) {
-        var asciiDigito = cnpjSemMascara.charCodeAt(i) - 48;
-        somatorioDV1 += asciiDigito * PESOS_DV[i + 1];
-        somatorioDV2 += asciiDigito * PESOS_DV[i];
+    if (!regexCaracteresNaoPermitidos.test(cnpj)) {
+      var cnpjSemMascara = removeMascaraCNPJ(cnpj);
+      if (regexCNPJSemDV.test(cnpjSemMascara) && cnpjSemMascara !== cnpjZerado.substring(0, tamanhoCNPJSemDV)) {
+        var somatorioDV1 = 0;
+        var somatorioDV2 = 0;
+        for (var i = 0; i < tamanhoCNPJSemDV; i++) {
+          var asciiDigito = cnpjSemMascara.charCodeAt(i) - valorBase;
+          somatorioDV1 += asciiDigito * pesosDV[i + 1];
+          somatorioDV2 += asciiDigito * pesosDV[i];
+        }
+        var dv1 = somatorioDV1 % 11 < 2 ? 0 : 11 - (somatorioDV1 % 11);
+        somatorioDV2 += dv1 * pesosDV[tamanhoCNPJSemDV];
+        var dv2 = somatorioDV2 % 11 < 2 ? 0 : 11 - (somatorioDV2 % 11);
+        return "" + dv1 + dv2;
       }
-
-      var dv1 = somatorioDV1 % 11 < 2 ? 0 : 11 - (somatorioDV1 % 11);
-      somatorioDV2 += dv1 * PESOS_DV[TAMANHO_CNPJ_SEM_DV];
-      var dv2 = somatorioDV2 % 11 < 2 ? 0 : 11 - (somatorioDV2 % 11);
-
-      return "" + dv1 + dv2;
     }
-
-    throw new Error("CNPJ inválido");
+    throw new Error("Não é possível calcular o DV pois o CNPJ fornecido é inválido");
   }
 
   function isValid(cnpj) {
-    var cnpjSemMascara = removeMascaraCNPJ(cnpj).toUpperCase();
-
-    if (REGEX_CNPJ.test(cnpjSemMascara) && cnpjSemMascara !== CNPJ_ZERADO) {
-      var dvInformado = cnpjSemMascara.substring(TAMANHO_CNPJ_SEM_DV);
-      var dvCalculado = calculaDV(cnpjSemMascara.substring(0, TAMANHO_CNPJ_SEM_DV));
-      return dvInformado === dvCalculado;
+    if (!regexCaracteresNaoPermitidos.test(cnpj)) {
+      var cnpjSemMascara = removeMascaraCNPJ(cnpj);
+      if (regexCNPJ.test(cnpjSemMascara) && cnpjSemMascara !== cnpjZerado) {
+        var dvInformado = cnpjSemMascara.substring(tamanhoCNPJSemDV);
+        var dvCalculado = calculaDV(cnpjSemMascara.substring(0, tamanhoCNPJSemDV));
+        return dvInformado === dvCalculado;
+      }
     }
     return false;
   }
 
   return {
     isValid: isValid,
-    calculaDV: calculaDV,
-    removeMascaraCNPJ: removeMascaraCNPJ
+    calculaDV: calculaDV
   };
-
 })();
